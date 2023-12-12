a737f72d9f57afbae37a21081f6b6d81cce24301
Jhon Anderson <72746231+jhonk17@users.noreply.github.com>
2023-12-12 15:09:30 -0300

    Atualização

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,26 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1];
+
+    //Template string
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+       
+    }
+    console.log(i);
+}
+
+
